8b683724017b4ffdb16026a49ae47e071ca957b2
ninishka <zambakhidzenini.740@gmail.com>
2024-04-25 14:00:51 +0400

    finished js first assignment

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+//number 
+let bike = 600;
+let Shipping = 200;
+let sum = bike + Shipping;
+// This will output the value of sum in the console
+console.log(sum);
+
+//subtraction
+let totalSubtraction = bike - Shipping;
+console.log(totalSubtraction)
+
+//multiplication
+let totalmultiplication = bike * Shipping;
+console.log(totalmultiplication)
+
+//division
+let totaldivision = bike / Shipping;
+console.log(totaldivision)
+
+//string/number
+let name1 = 'nini';
+let age = 20;
+let occupation = "front-end-developer"
+console.log('info ===', `Hello, i'm ${name1}, i'm ${age} years old ${occupation}`)
+console.log('info === ' + name1 + ' ' + age + ' ' + occupation);
+
+
+//calculated rectangle area 
+let length = 7;
+let width = 5;
+
+let area = length * width;
+console.log ("calculated area:" + area )
+
